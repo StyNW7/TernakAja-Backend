d1fc26bbcf82ffcf4fcbc8ba54e042745c100eeb
F4E12 <76392444+F4E12@users.noreply.github.com>
2025-05-26 07:30:51 +0700

    Update drizzle.ts

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { drizzle } from "drizzle-orm/neon-serverless";
 import dotenv from "dotenv";
 import { Pool } from "@neondatabase/serverless";
@@ -5,4 +6,19 @@ import { Pool } from "@neondatabase/serverless";
 dotenv.config();
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+=======
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+>>>>>>> Stashed changes
 export const db = drizzle(pool);
